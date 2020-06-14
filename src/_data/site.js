f18c3a5832c6f059d4694c96f9a4492f200f9d32
Lara Amalia <lara@scale.at>
2020-06-14 22:03:30 +0200

    Fix home link

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,11 +4,13 @@ module.exports = {
   name: "Lara Amalia",
   twitter: "@lara_amalia",
   en: {
+    homePath: "/en",
     metaTitle: "Lara Amalia. Web developer & musician.",
     metaDescription:
       "Hello, my name is Lara Amalia. I’m a web developer by day and a musician by night.",
   },
   de: {
+    homePath: "/",
     metaTitle: "Lara Amalia. Web Entwicklerin & Musikerin.",
     metaDescription:
       "Hallo, ich bin Lara Amalia. Ich bin Web Entwicklerin am Tag und Musikerin bei Nacht.",
