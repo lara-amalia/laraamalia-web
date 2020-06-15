03a92cee225a120df1c1e26140c1afbd1001f363
Lara Amalia <lara@scale.at>
2020-06-15 15:52:38 +0200

    Add language navigation

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,12 +5,14 @@ module.exports = {
   twitter: "@lara_amalia",
   en: {
     homePath: "/en/",
+    langNav: (link) => `<a href="${link.substring(3)}">DE</a> / <span>EN</span>`,
     metaTitle: "Lara Amalia. Web developer & musician.",
     metaDescription:
       "Hello, my name is Lara Amalia. I’m a web developer by day and a musician by night.",
   },
   de: {
     homePath: "/",
+    langNav: (link) => `<span>DE</span> / <a href="/en${link}">EN</a>`,
     metaTitle: "Lara Amalia. Web Entwicklerin & Musikerin.",
     metaDescription:
       "Hallo, ich bin Lara Amalia. Ich bin Web Entwicklerin am Tag und Musikerin bei Nacht.",
